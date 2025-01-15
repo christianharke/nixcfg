e3fe9a5970dd77a8dd6abcee0b466edf5051a7a8
Christian Harke <christian@harke.ch>
2025-01-16 00:38:33 +0100

    Configure power cycle notifications
    skip ci

--- nixos/roles/nas/default.nix
+++ nixos/roles/nas/default.nix
@@ -1,10 +1,18 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
 
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) getExe mkEnableOption mkIf;
 
   cfg = config.custom.roles.nas;
+
+  secretNtfyToken = "${config.custom.base.hostname}/ntfy-token";
+  secretNtfyUrl = "${config.custom.base.hostname}/ntfy-url";
 
 in
 
@@ -17,11 +25,18 @@ in
 
   config = mkIf cfg.enable {
     custom = {
-      base.system = {
-        boot.secureBoot = true;
-        btrfs.impermanence.enable = true;
-        luks.remoteUnlock = true;
-        network.wol.enable = true;
+      base = {
+        agenix.secrets = [
+          secretNtfyToken
+          secretNtfyUrl
+        ];
+
+        system = {
+          boot.secureBoot = true;
+          btrfs.impermanence.enable = true;
+          luks.remoteUnlock = true;
+          network.wol.enable = true;
+        };
       };
 
       roles.nas = {
@@ -29,6 +44,48 @@ in
         syncthing.enable = true;
       };
     };
+
+    powerManagement =
+      let
+        ntfyTopic = "chris-alerts";
+        mkNtfyCommand = body: ''
+          ${getExe pkgs.curl} \
+            -H "Authorization:Bearer $(${pkgs.coreutils}/bin/cat ${
+              config.age.secrets.${secretNtfyToken}.path
+            })" \
+            -d '${builtins.toJSON (body // { topic = ntfyTopic; })}' \
+            "$(${pkgs.coreutils}/bin/cat ${config.age.secrets.${secretNtfyUrl}.path})"
+        '';
+      in
+      {
+        powerDownCommands = mkNtfyCommand {
+          title = "Hyperion is going to save some power...";
+          message = "See you later!";
+          tags = [ "electric_plug" ];
+          actions = [
+            {
+              action = "view";
+              label = "Wake up";
+              url = "http://sv-syno-01:8090/";
+              clear = true;
+            }
+          ];
+        };
+
+        powerUpCommands = mkNtfyCommand {
+          title = "Hyperion is ready to serve data";
+          message = "Lets goo!";
+          tags = [ "floppy_disk" ];
+          actions = [
+            {
+              action = "view";
+              label = "Check status";
+              url = "http://hyperion:61208/";
+              clear = true;
+            }
+          ];
+        };
+      };
 
     services.glances = {
       enable = true;
